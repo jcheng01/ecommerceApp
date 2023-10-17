cb338d5e8c8e8fe7f5d057836e70078b4bb818b0
Justin Cheng <justincheng@Justins-MacBook-Pro.local>
2023-10-17 13:42:12 -0400

    nav ui setup

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { NavBar } from "../components/NavBar";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <NavBar />
+    </>
+  );
 };
 
 export default Home;
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import Button from "../components/Button";
 
 const Signup = () => {
   const [data, setData] = useState({});
